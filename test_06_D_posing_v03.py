86d118a623fb622a10d7a4445d3981f76e3f4735
uggoth <52624354+uggoth@users.noreply.github.com>
2022-11-19 15:18:20 +0000

    using latest objects
    tify up

--- test_06_D_posing_v03.py
+++ test_06_D_posing_v03.py
@@ -1,16 +1,19 @@
 # Posing Test and Setup
 
-import PicoBotF_v03 as ThisPico
+import ThisPico_F_v05 as ThisPico
 import utime
 
 module_name = 'test_06_D_posing_v03.py'
 
 print (module_name, "starting")
 
-bucket_servo = ThisPico.ThisBucketServo()
-shoulder_servo = ThisPico.ThisShoulderServo()
-
-my_arm = PicoBotF.ThisArm()
+my_arm = ThisPico.ThisArm()
+#my_arm.poses['PARK'] = [[my_arm.shoulder_servo,90],[my_arm.bucket_servo,90]]
+#my_arm.poses['UP'] = [[my_arm.shoulder_servo,100],[my_arm.bucket_servo,110]]
+#my_arm.poses['DUMP'] = [[my_arm.shoulder_servo,90],[my_arm.bucket_servo,155]]
+#my_arm.poses['CARRY'] = [[my_arm.shoulder_servo,90],[my_arm.bucket_servo,1]]
+#my_arm.poses['DOWN'] = [[my_arm.shoulder_servo,165],[my_arm.bucket_servo,10]]
+#my_arm.poses['SCOOP'] = [[my_arm.shoulder_servo,160],[my_arm.bucket_servo,1]]
 
 def do_pose(pose_id, speed):
     print (pose_id)
@@ -19,10 +22,14 @@ def do_pose(pose_id, speed):
 speed=100
 do_pose('PARK', speed)
 utime.sleep(1)
-do_pose('UP', speed)
-utime.sleep(2)
-do_pose('DOWN', speed)
-utime.sleep(2)
+#do_pose('DOWN', speed)
+#utime.sleep(1)
+#do_pose('SCOOP', speed)
+#utime.sleep(3)
+do_pose('CARRY', speed)
+utime.sleep(3)
+#do_pose('DUMP', speed)
+#utime.sleep(3)
 do_pose('PARK', speed)
 utime.sleep(1)
 
